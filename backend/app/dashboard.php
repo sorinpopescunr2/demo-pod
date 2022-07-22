b2725572e3e4cc4ab5dc7427ebc89716d573d667
sorinpopescunr2 <96417725+sorinpopescunr2@users.noreply.github.com>
2022-07-22 18:47:37 +0300

    Update dashboard.php

--- backend/app/dashboard.php
+++ backend/app/dashboard.php
@@ -28,8 +28,8 @@ activityLog(sprintf(
     <fieldset>
         <legend>Dashboard</legend>
         <ul>
-            <li><a href="/environ">Backend Environ 2022-07-21</a></li>
-            <li><a href="/logs">Container Logs 2022-07-21</a></li>
+            <li><a href="/environ">Backend Environ 2022-07-22</a></li>
+            <li><a href="/logs">Container Logs 2022-07-22</a></li>
         </ul>
     </fieldset>
 </body>
